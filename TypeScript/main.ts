991e1864c60e29b2e59ad3575b5cc65619d94405
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-07-30 21:37:12 -0300

    Refactor masking functions to consolidate CPF and CNPJ handling into a single function

--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -1,7 +1,5 @@
 import { isValidCPFOrCNPJ } from "./validate-cpf-or-cnpj";
-import { maskCPF } from "./mask-cpf";
-import { maskCNPJ } from "./mask-cnpj";
-import { maskNewCNPJ } from "./mask-new-cnpj";
+import { maskCPFOrCNPJ } from "./mask-cpf-or-cnpj";
 
 console.log("CPF com máscara:", isValidCPFOrCNPJ("123.456.789-09"));
 console.log("CPF sem máscara:", isValidCPFOrCNPJ("12345678909"));
@@ -12,8 +10,6 @@ console.log("CNPJ sem máscara:", isValidCPFOrCNPJ("11222333000181"));
 console.log("Novo CNPJ com máscara:", isValidCPFOrCNPJ("1A.23B.45C/678D-01"));
 console.log("Novo CNPJ sem máscara:", isValidCPFOrCNPJ("1A23B45C678D01"));
 
-console.log("CPF:", maskCPF("12345678909"));
-
-console.log("CNPJ:", maskCNPJ("11222333000181"));
-
-console.log("Novo CNPJ:", maskNewCNPJ("1A23B45C678D01"));
+console.log("CPF:", maskCPFOrCNPJ("12345678909"));
+console.log("CNPJ:", maskCPFOrCNPJ("11222333000181"));
+console.log("Novo CNPJ:", maskCPFOrCNPJ("1A23B45C678D01"));
--- TypeScript/mask-cpf-or-cnpj.ts
+++ TypeScript/mask-cpf-or-cnpj.ts
@@ -6,9 +6,18 @@ import { isValidCPF } from "./validate-cpf";
 import { isValidCNPJ } from "./validate-cnpj";
 import { isValidNewCNPJ } from "./validate-new-cnpj";
 
+type DocumentHandler = {
+  isValid: (doc: string) => boolean;
+  mask: (doc: string) => string;
+};
+
+const handlers: DocumentHandler[] = [
+  { isValid: isValidCPF, mask: maskCPF },
+  { isValid: isValidCNPJ, mask: maskCNPJ },
+  { isValid: isValidNewCNPJ, mask: maskNewCNPJ },
+];
+
 export const maskCPFOrCNPJ = (doc: string): string | null => {
-  if (isValidCPF(doc)) return maskCPF(doc);
-  if (isValidCNPJ(doc)) return maskCNPJ(doc);
-  if (isValidNewCNPJ(doc)) return maskNewCNPJ(doc);
-  return null;
+  const handler = handlers.find(({ isValid }) => isValid(doc));
+  return handler ? handler.mask(doc) : null;
 };
