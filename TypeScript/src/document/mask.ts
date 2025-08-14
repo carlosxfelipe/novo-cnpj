0a5ad990ff4e8d4d41db6718f6d93ceacff28550
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-08-14 20:54:02 -0300

    Fix return type of formatToCPFOrCNPJ function to always return a string; update invalid document response

--- TypeScript/src/document/mask.ts
+++ TypeScript/src/document/mask.ts
@@ -3,10 +3,10 @@ import { formatToCNPJ } from "../cnpj/mask";
 import { formatToCNPJX } from "../cnpjx/mask";
 import { whichDoc } from "./validate";
 
-export const formatToCPFOrCNPJ = (doc: string): string | null => {
+export const formatToCPFOrCNPJ = (doc: string): string => {
   const kind = whichDoc(doc);
   if (kind === "cpf") return formatToCPF(doc);
   if (kind === "cnpj") return formatToCNPJ(doc);
   if (kind === "cnpjx") return formatToCNPJX(doc);
-  return null;
+  return "Documento inválido";
 };
